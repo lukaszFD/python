e7b76e6f342126216ade532d4c9b8627d37d0767
Łukasz <lukasz.f.d@gmail.com>
2024-12-28 14:39:09 +0100

    Delete icmp_sniffer.py

--- scapy_tests/icmp_sniffer.py
+++
@@ -1,8 +0,0 @@
-from scapy.all import *
-from scapy.layers.inet import ICMP
-
-
-def packet_callback(packet):
-    if ICMP in packet and packet[ICMP].type == 8: # Typ 8 to echo request ICMP print(f"Source: {packet[IP].src}, Load: {packet[ICMP].load}")
-
-if name == "main": # Uruchomienie sniffing'u z filtrem ICMP typu echo request sniff(filter="icmp and icmp[0]=8", prn=packet_callback)
